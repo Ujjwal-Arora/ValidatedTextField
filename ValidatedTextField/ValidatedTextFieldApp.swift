ed3e1cb508028edfccbda36e266eb37565d5dcac
Ujjwal Arora <ukarora9@gmail.com>
2024-12-13 12:25:57 +0530

    Initial Commit

---
+++ ValidatedTextField/ValidatedTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidatedTextFieldApp.swift
+//  ValidatedTextField
+//
+//  Created by Ujjwal Arora on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ValidatedTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
